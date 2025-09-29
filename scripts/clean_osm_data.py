bba7490973aeaba1547ea7b15a05ba2bc44a462d
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-09-29 11:19:11 +0200

    Generalize LineString

--- scripts/clean_osm_data.py
+++ scripts/clean_osm_data.py
@@ -730,7 +730,7 @@ def filter_lines_by_geometry(df_all_lines):
     df_all_lines.dropna(subset=["geometry"], axis=0, inplace=True)
 
     # remove lines represented as Polygons
-    df_all_lines = df_all_lines[df_all_lines.geometry.geom_type == "LineString"]
+    df_all_lines = df_all_lines[df_all_lines.geometry.geom_type.str.contains("LineString")]
 
     return df_all_lines
 

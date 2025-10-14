208ea46122a849d7c833379b4e780a3986d1ac44
Davide Fioriti <fioritidavidesubs@gmail.com>
2025-10-14 09:17:52 +0200

    Add geojson export to build_osm_network

--- scripts/build_osm_network.py
+++ scripts/build_osm_network.py
@@ -15,11 +15,12 @@ from _helpers import (
     create_logger,
     read_geojson,
     read_osm_config,
+    save_to_geojson,
     to_csv_nafix,
 )
 from scipy.spatial import cKDTree
 from shapely.geometry import LineString, MultiLineString, Point
-from shapely.ops import linemerge, nearest_points, split, snap
+from shapely.ops import linemerge, nearest_points, snap, split
 from sklearn.cluster import DBSCAN
 from tqdm import tqdm
 
@@ -37,6 +38,18 @@ LINES_COLUMNS = [
     "bus1",
     "length",
     "dc",
+    "geometry",
+]
+CONVERTERS_COLUMNS = [
+    "converter_id",
+    "bus0",
+    "bus1",
+    "geometry",
+]
+TRANSFORMERS_COLUMNS = [
+    "line_id",
+    "bus0",
+    "bus1",
     "geometry",
 ]
 
@@ -504,7 +517,7 @@ def merge_stations_lines_by_station_id_and_voltage(
 
     # drop lines starting and ending in the same node
     lines.drop(lines[lines["bus0"] == lines["bus1"]].index, inplace=True)
-    
+
     # update line endings
     lines = line_endings_to_bus_conversion(lines)
 
@@ -735,7 +748,6 @@ def built_network(
     countries_config,
     geo_crs,
     distance_crs,
-    lines_cols_standard,
     force_ac=False,
 ):
     logger.info("Stage 1/5: Read input data")
@@ -800,17 +812,23 @@ def built_network(
     if not os.path.exists(outputs["lines"]):
         os.makedirs(os.path.dirname(outputs["lines"]), exist_ok=True)
 
-    lines = lines[lines_cols_standard]
+    lines = lines[LINES_COLUMNS]
+    converters = converters[CONVERTERS_COLUMNS]
+    transformers = transformers[TRANSFORMERS_COLUMNS]
 
-    to_csv_nafix(lines, outputs["lines"])  # Generate CSV
-    to_csv_nafix(converters, outputs["converters"])  # Generate CSV
-    to_csv_nafix(transformers, outputs["transformers"])  # Generate CSV
+    to_csv_nafix(lines, outputs["lines"])
+    save_to_geojson(lines, outputs["lines_geo"])
+    to_csv_nafix(converters, outputs["converters"])
+    save_to_geojson(converters, outputs["converters_geo"])
+    to_csv_nafix(transformers, outputs["transformers"])
+    save_to_geojson(transformers, outputs["transformers_geo"])
 
     # create clean directory if not already exist
     if not os.path.exists(outputs["substations"]):
         os.makedirs(os.path.dirname(outputs["substations"]), exist_ok=True)
-    # Generate CSV
+
     to_csv_nafix(buses, outputs["substations"])
+    save_to_geojson(buses, outputs["substations_geo"])
 
     return None
 
@@ -837,6 +855,5 @@ if __name__ == "__main__":
         countries,
         geo_crs,
         distance_crs,
-        lines_cols_standard=LINES_COLUMNS,
         force_ac=force_ac,
     )
